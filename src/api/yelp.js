bf2b82a42ac305ad476eb0bde88fecf81fe03d22
İsmail İLGÖR <83916341+ismaililgor@users.noreply.github.com>
2023-12-27 23:35:23 +0300

    Update yelp.js For Secure API KEY

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create ({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer rBWjYx_WQkpnei4ro0yxGYw8TeJQtO1crhVPC5nUH_O-UF6ky_ARwuSf6ZFzDdfWQ3_cnucrBWWjCt9CGAgizKEQR0A6iXCw3F1LUrNL5MLWYY5ZimzvEACCiJ1rZXYx'
+        Authorization: 'Bearer **APIKEY**'
     }
 })
 
